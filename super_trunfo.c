79cda872abd63adcfa01114bd3966d847c6315a1
leobocks <leobocks@gmail.com>
2025-09-08 09:23:04 -0300

    Update super_trunfo.c
    Desafio nível intermediário com cálculos de densidade populacional e renda per capta

--- super_trunfo.c
+++ super_trunfo.c
@@ -11,9 +11,15 @@ int main() {
  int populacao, turistico, populacao2, turistico2;
  float area, area2; 
  float pib, pib2;
+ float densidade, densidade2;
+ float rendaper, rendaper2;   
  char estado, estado2;
  char codigocidade[10], codigocarta[10], nomecidade[10];
  char codigocidade2[10], codigocarta2[10], nomecidade2[10];
+ densidade = populacao / area;
+ densidade2 = populacao2 / area2;
+ rendaper = pib / populacao;
+ rendaper2 = pib2 / populacao2;    
     
     // Cadastro das Cartas:
     // Esta seção solicita que o usuário preencha os dados
@@ -40,7 +46,6 @@ int main() {
 
  printf("Digite o número de pontos turísticos: \n");
  scanf("%d", &turistico);
-
   
  printf("Digite uma letra para representar o Estado: \n");
  scanf(" %c", &estado2);
@@ -62,7 +67,7 @@ int main() {
 
  printf("Digite o número de pontos turísticos: \n");
  scanf("%d", &turistico2);
-printf("\n\n");
+ printf("\n\n");
     // Exibição dos Dados das Cartas:
     // Serão preenchidas 2 cartas sequencialmente
     // Os valores inseridos para cada atributo da cidade serão exibidos um por linha.
@@ -75,7 +80,11 @@ printf("\n\n");
  printf("Área em km²: %f \n", area);
  printf("PIB: %f \n", pib);
  printf("Número de pontos turísticos: %d \n\n", turistico);
-
+ densidade = populacao / area;
+ printf("Densidade Populacional: %.2f hab/km²\n", densidade);
+ rendaper = pib / populacao;   
+ printf("PIB per Capta: %.2f reais\n", rendaper);
+      
  printf("Carta 2 \n");
  printf("Estado: %c \n", estado2);
  printf("Código: %s \n", codigocarta2);
@@ -84,6 +93,10 @@ printf("\n\n");
  printf("Área em km²: %f \n", area2);
  printf("PIB: %f \n", pib2);
  printf("Número de pontos turísticos: %d \n", turistico2);
+ densidade2 = populacao2 / area2;   
+ printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+ rendaper2 = pib2 / populacao2;    
+ printf("PIB per Capta: %.2f reais\n", rendaper2);
 
  return 0;
 }
