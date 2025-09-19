8189c9ece47ce409cc526677263bfffd0e03b6a7
leobocks <leobocks@gmail.com>
2025-09-19 11:31:44 -0300

    Update super_trunfo.c
    Dados de população agora estão em unsigned long int e aceitam números maiores. Foi criado um comparativo entre as cartas.

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - novato
+// Desafio Super Trunfo - mestre
 // Como cadastrar cartas
 // Siga as indicações para cadastrar as cartas
 
 
 
 int main() {
-    // Estado é representado apenas por uma letra
- int populacao, turistico, populacao2, turistico2;
- float area, area2; 
+    // Estado é representado apenas por um caractere
+    // População agora aceita números maiores
+ int turistico, turistico2;
+ unsigned long int populacao, populacao2;
+ float area, area2;
  float pib, pib2;
+ float superpoder, superpoder2;
  float densidade, densidade2;
- float rendaper, rendaper2;   
+ float rendaper, rendaper2;  
  char estado, estado2;
  char codigocidade[10], codigocarta[10], nomecidade[10];
  char codigocidade2[10], codigocarta2[10], nomecidade2[10];
@@ -36,7 +39,7 @@ int main() {
  scanf("%s", &nomecidade);
 
  printf("Digite a população: \n");
- scanf("%d", &populacao);
+ scanf("%ld", &populacao);
 
  printf("Digite a área em km²: \n");
  scanf("%f", &area);
@@ -57,7 +60,7 @@ int main() {
  scanf("%s", &nomecidade2);
 
  printf("Digite a população: \n");
- scanf("%d", &populacao2);
+ scanf("%ld", &populacao2);
 
  printf("Digite a área em km²: \n");
  scanf("%f", &area2);
@@ -68,6 +71,7 @@ int main() {
  printf("Digite o número de pontos turísticos: \n");
  scanf("%d", &turistico2);
  printf("\n\n");
+ 
     // Exibição dos Dados das Cartas:
     // Serão preenchidas 2 cartas sequencialmente
     // Os valores inseridos para cada atributo da cidade serão exibidos um por linha.
@@ -79,11 +83,11 @@ int main() {
  printf("População: %d \n", populacao);
  printf("Área em km²: %f \n", area);
  printf("PIB: %f \n", pib);
- printf("Número de pontos turísticos: %d \n\n", turistico);
+ printf("Número de pontos turísticos: %d \n", turistico);
  densidade = populacao / area;
  printf("Densidade Populacional: %.2f hab/km²\n", densidade);
  rendaper = pib / populacao;   
- printf("PIB per Capta: %.2f reais\n", rendaper);
+ printf("PIB per Capta: %.2f reais\n\n", rendaper);
       
  printf("Carta 2 \n");
  printf("Estado: %c \n", estado2);
@@ -96,7 +100,51 @@ int main() {
  densidade2 = populacao2 / area2;   
  printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
  rendaper2 = pib2 / populacao2;    
- printf("PIB per Capta: %.2f reais\n", rendaper2);
+ printf("PIB per Capta: %.2f reais\n\n", rendaper2);
+ 
+       // Cálculo do Super Poder
+      // Superpoder é a soma de todos os atributos mais o inverso da densidade - quanto menor a densidade, maior o poder
+       
+    superpoder = populacao + area + pib + turistico + rendaper + (1.0 / densidade);
+    superpoder2 = populacao2 + area2 + pib2 + turistico2 + rendaper2 + (1.0 / densidade2);
+ 
+    // Comparação das cartas - usando if e else
+  printf("\nComparação de Cartas:\n");
+    printf("População: ");
+    if (populacao > populacao2)
+        printf("Carta 1 venceu (1)\n");
+    else
+        printf("Carta 2 venceu (0)\n");
+    printf("Área: ");
+    if (area > area2)
+        printf("Carta 1 venceu (1)\n");
+    else
+        printf("Carta 2 venceu (0)\n");
+    printf("PIB: ");
+    if (pib > pib2)
+        printf("Carta 1 venceu (1)\n");
+    else
+        printf("Carta 2 venceu (0)\n");
+    printf("Pontos Turísticos: ");
+    if (turistico > turistico2)
+        printf("Carta 1 venceu (1)\n");
+    else
+        printf("Carta 2 venceu (0)\n");
+    printf("Densidade Populacional: ");
+    if (densidade < densidade2)
+        printf("Carta 1 venceu (1)\n");
+    else
+        printf("Carta 2 venceu (0)\n");
+    printf("PIB per Capita: ");
+    if (rendaper > rendaper2)
+        printf("Carta 1 venceu (1)\n");
+    else
+        printf("Carta 2 venceu (0)\n");
+    printf("Super Poder: ");
+    if (superpoder > superpoder2)
+        printf("Carta 1 venceu (1)\n");
+    else
+        printf("Carta 2 venceu (0)\n");
 
  return 0;
 }
